dcd407851996fea10975057983247425ee46093d
Begiims <attokurovabegimaj@gmail.com>
2022-12-10 20:08:51 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a == 1) {
+            System.out.println("math lessons");
+        } else if (a == 2) {
+            System.out.println("java lessons");
+        } else if (a == 3) {
+            System.out.println("literature");
+        } else if (a == 4) {
+            System.out.println("russian lessons");
+        } else if (a == 5) {
+            System.out.println("geography lessons");
+        } else if (a == 6) {
+            System.out.println("english lessons");
+        } else if (a == 7) {
+            System.out.println("ooh yeah holiday");
+        } else if (a == 8) {
+            System.out.println("ooh no yesterday school");
+
+
+        }
+
+    }
+}
+
+
